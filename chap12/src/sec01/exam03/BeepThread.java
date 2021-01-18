5ecdeeee438c9b5bd1effd48867f548bc6d0c0b5
kokiyo1030 <kokiyo1030@naver.com>
2021-01-19 00:48:25 +0900

    source

---
+++ chap12/src/sec01/exam03/BeepThread.java
@@ -0,0 +1,17 @@
+package sec01.exam03;
+
+import java.awt.Toolkit;
+
+public class BeepThread extends Thread {
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+
+			}
+		}
+	}
+}
